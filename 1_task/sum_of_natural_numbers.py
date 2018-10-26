c0cde8cbe8ccf468848c80dc7b891ea53e8a7447
nihal003 <44443605+nihal003@users.noreply.github.com>
2018-10-26 22:39:19 +0530

    Update sum_of_natural_numbers.py

--- 1_task/sum_of_natural_numbers.py
+++ 1_task/sum_of_natural_numbers.py
@@ -1,9 +1,7 @@
-#Initialize variable "sum" as 0.
-#variable "i" is given in a loop from 4 to 1000 to check if it is divisible by 5 or 4.if yes then "i" is added to "sum"
-#After the loop print the output as "sum".
-sum=0
 
-for i in range(4,1000):
+sum=0 #Initialize variable "sum" as 0.
+
+for i in range(4,1000): #variable "i" is given in a loop from 4 to 1000 to check if it is divisible by 5 or 4
     if i%5==0 or i%4==0:
-        sum=sum+i
-print("sum=",sum)
+        sum=sum+i #if yes then "i" is added to "sum"
+print("sum=",sum) #After the loop print the output as "sum".
