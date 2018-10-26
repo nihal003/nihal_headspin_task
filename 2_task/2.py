03bf0cfd2d3b33b68272f91299da0a4d7a5bb6b7
nihal003 <44443605+nihal003@users.noreply.github.com>
2018-10-26 22:58:19 +0530

    Update 2.py

--- 2_task/2.py
+++ 2_task/2.py
@@ -2,13 +2,13 @@
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 
-import fractions
+import fractions                    #Imported the module fractions for using built in function gcd() used to find greatest common divisor (gcd)
 
 def lcm(n):
     x=1
     for i in range(1,n+1):
-        x=(x*i)/fractions.gcd(x,i)
+        x=(x*i)/fractions.gcd(x,i)  #Initialised variable "x" as "i" is used to calculate the formula "x=(x*i)/fractions.gcd(x,i)" in a loop from 1 to 20.
     return x
 
 ans=lcm(20)
-print(ans)
+print(ans)                         #After the loop "x" is printed for the output
